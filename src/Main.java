2708e8e21430702ce9c8df7182e8a6c70a95fa72
SerafinVelEnc <115732895+SerafinVelEnc@users.noreply.github.com>
2022-11-10 23:05:47 +0100

    Subir diagrama de clases.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Iniciando...");
+        /**
+         * Introduccion de libros por el bibliotecario.
+         */
+
+        Scanner entrada=new Scanner(System.in);
+        Libros libro = new Libros();
+
     }
 }
